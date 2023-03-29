d3549f2fcbfba14404ebc98b85855453f36b3ef3
ARTURO RIOS <arturo.rioscueto@gmail.com>
2023-03-29 16:22:32 -0600

    se modifica el archivo main con una validacion

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 import clases.User
+import java.util.*
 
 fun main(args: Array<String>) {
 
@@ -7,39 +8,37 @@ fun main(args: Array<String>) {
     do {
         println()
         println("************************************")
-        println(" Bienvenido a  *** SuperTianguis ***")
+        println(" Bienvenido   *** SuperTianguis ***")
         println("************************************")
         println(" MENU ")
         println(" 1.- Registrar Usuario ")
         println(" 2.- Iniciar Session")
         println(" 3.- listar Usuarios")
         println(" 4.- Salir de aplicación")
-        // val opcion = readLine()!!.toString()
-        val opcion  = readLine()?.toInt() as Int
-        when ( opcion ){
-            1 -> {
-                println("Registrar usuario nuevo")
-                user.altaUser()
-            }
-            2 -> {
-                println(" Iniciar Session")
-                user.login()
-                break
-            }
-            3 -> {
-                user.listUsers()
-            }
-            4 -> {
-                println(" Salir...")
-                break
+        try {
+            val opcion  = readLine()?.toInt() as Int
+            when ( opcion ){
+                1 -> {
+                    println("Registrar usuario nuevo")
+                    user.altaUser()
+                }
+                2 -> {
+                    println(" Iniciar Session")
+                    user.login()
+                }
+                3 -> {
+                    user.listUsers()
+                }
+                else -> {
+                    println(" Closing Program...")
+                    break
+                }
+            }  // when
+        } catch (ex: InputMismatchException){
+            println("Enter valid number")
+        } catch (ex: NumberFormatException){
+            println("Seleccionar una opcion valida  por favor")
+        }
 
-            }
-            else -> {
-                println(" salir...")
-                break
-            }
-
-        }  // when
-
-    } while(!opcion.equals(4) )
+    } while(true )
 }
